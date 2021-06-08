8bd7164f089542891fa8072038ce212c1ef5d465
Christopher <calb200@aucklanduni.ac.nz>
2021-06-09 09:30:05 +1200

    style navbar search

--- client/components/JCUX/Container/Container.jsx
+++ client/components/JCUX/Container/Container.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./container.css";
 
 const JCUXContainer = (props) => {
-  return <div className="jcux-container">{props.children}</div>;
+  return (
+    <div className={"jcux-container " + props.className}>{props.children}</div>
+  );
 };
 
 export default JCUXContainer;
--- client/components/navbar/Navbar.jsx
+++ client/components/navbar/Navbar.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { InputGroup } from "react-bootstrap";
 import Container from "../JCUX/Container";
 import "./navbar.css";
+
+const onSearchbarFocus = () => {
+  let form = document.getElementsByClassName("jcux-navbar-searchbar")[0];
+  form.style.boxShadow = "0px 0px 10px 5px white";
+};
+
+const onSearchbarLoseFocus = () => {
+  let form = document.getElementsByClassName("jcux-navbar-searchbar")[0];
+  form.style.boxShadow = "none";
+};
+
 const NavbarMain = () => {
+  useEffect(() => {
+    let input = document.getElementsByClassName("jcux-navbar-input")[0];
+    input.addEventListener("focusin", onSearchbarFocus);
+    input.addEventListener("focusout", onSearchbarLoseFocus);
+  });
   return (
-    <div className="navbar-main-outer">
-      <Container>
-        <h1>eCommerce V3</h1>
+    <div className="jcux-navbar-main-outer">
+      <Container className="jcux-navbar-container">
+        <h1 className="jcux-navbar-brand jcux-navbar-item">eCommerce V3</h1>
+        <h2 className="jcux-navbar-link jcux-navbar-item">Home</h2>
+        <h2 className="jcux-navbar-link jcux-navbar-item">Shop</h2>
+        <form className="jcux-navbar-searchbar jcux-navbar-item" action="/">
+          <input
+            className="jcux-navbar-input"
+            type="text"
+            placeholder="search for your item"
+          />
+          <button className="jcux-navbar-submit" type="submit">
+            <i className="fas fa-search"></i>
+          </button>
+        </form>
       </Container>
     </div>
   );
