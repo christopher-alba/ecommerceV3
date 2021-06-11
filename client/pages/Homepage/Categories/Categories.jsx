5cf6d1b114ae7bf3f7c87bc9b553f66c7232ccff
Christopher <calb200@aucklanduni.ac.nz>
2021-06-12 10:46:16 +1200

    set image urls for categories

--- client/pages/Homepage/Categories/Categories.jsx
+++ client/pages/Homepage/Categories/Categories.jsx
@@ -10,20 +10,24 @@ const Categories = () => {
         <Title text="Categories" />
         <div className="categories-main-wrapper">
           <CategoryBlock
-            src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"
+            src="https://meowprint.sg/wp-content/uploads/2018/12/Plain-t-shirts-wallpaper.jpg"
             text="T-SHIRTS"
+            location="/products/tshirts"
           />
           <CategoryBlock
-            src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"
+            src="https://images.unsplash.com/photo-1565084888279-aca607ecce0c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVuaW18ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80"
             text="PANTS"
+            location="/products/pants"
           />
           <CategoryBlock
-            src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"
+            src="https://cdn.luxe.digital/media/2021/04/30134651/best-leather-men-jacket-maker-ionic-review-luxe-digital%402x.jpg"
             text="JACKETS"
+            location="/products/jackets"
           />
           <CategoryBlock
-            src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"
+            src="https://cdn.cnn.com/cnnnext/dam/assets/210602121042-064bs-hanes-womens-stretch-jersey-bike-short.jpg"
             text="SHORTS"
+            location="/products/shorts"
           />
         </div>
       </Container>
--- client/pages/Homepage/Categories/CategoryBlock/CategoryBlock.jsx
+++ client/pages/Homepage/Categories/CategoryBlock/CategoryBlock.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./categoryBlock.css";
 const CategoryBlock = (props) => {
+  const onButtonClick = () => {
+    window.location.replace(props.location);
+  };
   return (
-    <div className="category-button">
+    <div className="category-button" onClick={onButtonClick}>
       <img className="category-image" src={props.src} alt="" />
       <div className="category-image-filter" />
       <span className="category-text">{props.text}</span>
