600002e1800116725493c9ea13193461b635c4ab
Christopher <calb200@aucklanduni.ac.nz>
2021-06-12 10:27:04 +1200

    add filter to category block

--- client/pages/Homepage/Categories/CategoryBlock/CategoryBlock.jsx
+++ client/pages/Homepage/Categories/CategoryBlock/CategoryBlock.jsx
@@ -4,6 +4,7 @@ const CategoryBlock = (props) => {
   return (
     <div className="category-button">
       <img className="category-image" src={props.src} alt="" />
+      <div className="category-image-filter" />
       <span className="category-text">{props.text}</span>
     </div>
   );
